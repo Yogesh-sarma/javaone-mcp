bd6750128634b810a9aa34f89e3c2091d1364161
yogesh-sarma <addankiyogesh@gmail.com>
2025-09-20 17:39:09 -0500

    Created presentations and a server

---
+++ src/main/java/dev/yo/Main.java
@@ -0,0 +1,62 @@
+package dev.yo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.modelcontextprotocol.server.McpServer;
+import io.modelcontextprotocol.server.McpServerFeatures;
+import io.modelcontextprotocol.server.McpSyncServer;
+import io.modelcontextprotocol.server.transport.StdioServerTransportProvider;
+import io.modelcontextprotocol.spec.McpSchema;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+    private static PresentationTools presentationTools = new PresentationTools();
+
+    public static void main(String[] args){
+        // stdio server transport
+        var transportProvider = new StdioServerTransportProvider(new ObjectMapper());
+        var syncToolSpecification = getSyncToolSpecification();
+
+        McpSyncServer syncServer = McpServer.sync(transportProvider)
+                .serverInfo("javaone-mcp-server", "0.0.1")
+                .capabilities(McpSchema.ServerCapabilities.builder()
+                        .tools(true)
+                        .logging()
+                        .build())
+                .tools(syncToolSpecification)
+                .build();
+
+        log.info("starting server...");
+    }
+
+    private static McpServerFeatures.SyncToolSpecification getSyncToolSpecification(){
+        var schema = """
+            {
+              "type" : "object",
+              "id" : "urn:jsonschema:Operation",
+              "properties" : {
+                "operation" : {
+                  "type" : "string"
+                }
+              }
+            }
+            """;
+
+        return new McpServerFeatures.SyncToolSpecification(
+                new McpSchema.Tool("get_presentations", "Get a list of all presentations from JavaOne", schema),
+                (exchange, arguments) -> {
+                    // Tool implementation
+                    List<Presentation> presentations = presentationTools.getPresentations();
+                    List<McpSchema.Content> contents = new ArrayList<>();
+                    for (Presentation presentation : presentations) {
+                        contents.add(new McpSchema.TextContent(presentation.toString()));
+                    }
+                    return new McpSchema.CallToolResult(contents, false);
+                }
+        );
+    }
+}
---
+++ src/main/java/dev/yo/Presentation.java
@@ -0,0 +1,4 @@
+package dev.yo;
+
+public record Presentation(String title, String url, int year) {
+}
